5ad016fa1058fd710bad1bdb923844f7a232fe0a
heaveek <heaveek@gmail.com>
2018-09-25 16:07:47 +0300

    Exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	if(currency > 10000)
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    // create an object with a list of changeable coins
+    const coinObj = {
+        'H': 50,
+        'Q': 25,
+        'D': 10,
+        'N': 5,
+        'P': 1
+    }
+    const result = {};
+    for(let key in coinObj){       
+        let amount = Math.trunc(currency / coinObj[key]); // .trunc found on the habr :)
+        if(amount > 0) {
+            result[key] = amount;
+            currency = currency % coinObj[key]; // change the argument to remainder
+        }
+    }    
+    return result;
 }
